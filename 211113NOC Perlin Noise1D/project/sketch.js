e860bda7123b138ede7f1dc838a4ad8c07a5e252
Cheeeeeeer <90513944+Cheeeeeeer@users.noreply.github.com>
2021-11-13 16:11:42 -0500

    Update sketch.js

--- 211113NOC Perlin Noise1D/project/sketch.js
+++ 211113NOC Perlin Noise1D/project/sketch.js
@@ -1,5 +1,5 @@
-let off=0;
-let startValue = 0;//change the off start
+let xoff=0;
+let startValue = 0;//change the xoff start
 
 function setup(){
 createCanvas(600,600);
@@ -8,13 +8,13 @@ noFill();
 
 function draw(){
 background(0);
-off = startValue; //it looks like a hearbeat rate cuz off is valued in the draw function
+xoff = startValue; //it looks like a hearbeat rate cuz xoff is valued in the draw function
 beginShape();
 for (x=0;x<width;x++){
   stroke(255);
-  let y = noise(off)*height;
+  let y = noise(xoff)*height;
   vertex(x,y);
-  off+=0.01;
+  xoff+=0.01;
 }
 endShape();
 startValue+=0.01;
